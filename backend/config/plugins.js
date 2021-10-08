39e9b7d356414bf19a845ba5d90570bac02c7419
Juseong Kim <dcjuja@gmail.com>
2021-10-09 01:44:36 +0900

    🎉 문서 추가

---
+++ backend/config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = {
+  upload: {
+    provider: 'google-cloud-storage',
+    providerOptions: {
+      bucketName: 'js-2021-328409.appspot.com',
+      publicFiles: false,
+      uniform: false,
+      basePath: '',
+    },
+  },
+  //...
+}
