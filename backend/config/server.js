d13c4f9e80dfbd6e60fc21b614599de1b5c8a7f5
Juseong Kim <dcjuja@gmail.com>
2021-10-08 18:45:33 +0900

    🎉 프로젝트 시작

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '66c0ed7f37dfeba539188366a0ee138d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '117cbd97-697d-4436-a3c6-2adce168bd43'
+};
